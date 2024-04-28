2369c96320dce0b2475933f94030c6cbb49890df
evie-8 <neouma29@gmail.com>
2024-04-28 08:48:15 +0300

    task 0 corrections

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,17 +52,20 @@ def before_request() -> str:
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/']
+        ]
 
-    if auth:
-        if auth.require_auth(request.path, request_path_list):
-            if auth.authorization_header(request) is None:
-                abort(401)
-            request.current_user = auth.current_user(request)
-            if auth.current_user(request) is None:
-                abort(403)
-            if request.current_user is None:
-                abort(403)
+    if auth is None:
+        return
+    if auth.require_auth(request.path, request_path_list) is False:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
